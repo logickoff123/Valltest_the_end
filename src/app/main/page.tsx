f95b810fef18e1d0af3ef0830074ccf3787d8f7e
logickoff123 <SonyaUstinova@gmail.com>
2025-11-04 18:25:02 +0300

    this id the end

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,5 @@
 import styles from "./mainPage.module.css";
-import { StartMain } from "@/components/main/StartMain";
-import { CarouselMain } from "@/components//main/CarouselMain"
-import { WhyVallTestMain } from "@/components/main/WhyVallTestMain";
-import { RoadmapMain } from "@/components/main/RoadmapMain";
-import { ContactsMain } from "@/components/main/ContactsMain";
+
 
 
 export default function MainContainers() {
@@ -12,11 +8,6 @@ export default function MainContainers() {
       <div>
         <h1 className={styles.heading}>Главная</h1>
         <div className={styles.content}>
-          <StartMain />
-          <CarouselMain />
-          <WhyVallTestMain />
-          <RoadmapMain />
-          <ContactsMain />
         </div>
       </div>
     </main>
